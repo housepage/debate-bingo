852460f67567be82850f975f26da0dbf59c1817c
Andrew Edward Gall <housepage@gmail.com>
2012-10-02 17:52:40 -0700

    Final tweaks before release

---
+++ README.md
@@ -0,0 +1,16 @@
+*Dependencies*
+
+* LaTex
+* Metapost
+* bbcards package for Metapost
+* ps2pdf (Optional) 
+
+*Instructions*
+
+# Compose file with each square you want on a separate line
+# Call './debate-bingo.bash \[ input filename \] \[ output filename \]' or pipe a new line seperate list to './debate-bingo.bash'
+# Print the prepared bingo sheet that appears in your folder as the name you specified or 'debate-bingo.pdf' or (debate-bingo.ps if you don't have ps2pdf)
+
+*Acknowledgements*
+
+* Based heavily on the [bbcard package](http://www.ctan.org/pkg/bbcard)
---
+++ debate-bingo.bash
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+declare -a terms array
+
+IFS=$'\x0a';
+
+if [ $# -lt 1 ]; then
+  while read -r line; do  terms+=( "$line" ); done
+  
+  count=${#terms[@]}
+
+  if [ $count -lt 24 ]; then
+    echo "Must have at least 24 entries"
+    exit
+  fi
+  
+  mp_command=""
+  array_create="string bs[]; "
+  mp_command+="$array_create";
+
+  i=1;
+
+  declare -a escape_these=("'" "\"");
+
+  for term in ${terms[@]}; do
+
+    for j in ${escape_these[@]}; do
+      term=${term//$j/\\$j}
+    done
+
+    mp_command+="bs[$i] := \"$term\"; ";
+    let "i+=1";
+  done;
+
+  mp_command+=" bsmax := $count; "
+  mp_command+="input bbcard"
+
+  command_string="mpost '\\$mp_command'"
+
+  eval $command_string
+  
+else
+  while read -r line; do  terms+=( "$line" ); done < $1
+  
+  count=${#terms[@]}
+
+  if [ $count -lt 24 ]; then
+    echo "Must have at least 24 entries"
+    exit
+  fi
+
+  command_string=$(printf "mpost '\string file; file := \"%s\"; input debate-bingo;'" $1)
+
+  eval $command_string
+  
+  if [ $# -lt 2 ]; then
+    ps2pdf debate-bingo.ps $2
+    exit
+  fi
+fi
+
+ps2pdf debate-bingo.ps
+
